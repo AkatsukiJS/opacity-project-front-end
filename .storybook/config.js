c3c460f9c78663d09c316c5ed9344dd3446498a4
Jeova <jeovapereira2000@live.com>
2019-02-09 17:13:15 -0300

    chore: config storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,42 @@
-import { configure } from '@storybook/react';
+import theme from '../src/theme.js'
+import React from 'react'
+import { withKnobs } from '@storybook/addon-knobs'
+import { withInfo } from '@storybook/addon-info'
+import { ThemeProvider } from 'emotion-theming'
+import { configure, addDecorator, addParameters } from '@storybook/react'
 
-function loadStories() {
-  require('../src/components/atoms/stories.js');
+import './index.css'
+
+addDecorator(
+  withInfo({
+    inline: true,
+    header: false,
+    styles: {
+      infoBody: {
+        fontFamily: 'Ropa Sans',
+        border: '1px solid #eee',
+        padding: '20px 40px 40px'
+      }
+    }
+  })
+)
+
+addDecorator(
+  story => <ThemeProvider theme={theme}>{story()}</ThemeProvider>
+)
+
+const req = require.context('../src/components', true, /.stories.js$/)
+function loadStories () {
+  req.keys().forEach(filename => req(filename))
 }
 
-configure(loadStories, module);
+addDecorator(withKnobs)
+
+addParameters({
+  options: {
+    name: 'Opacity Project',
+    addonPanelInRight: true
+  }
+})
+
+configure(loadStories, module)
