80905a84df122b37b0fd2ab72f2d9c63f49796d1
Jeova <jeovapereira2000@live.com>
2019-02-18 00:21:29 -0300

    chore: add RadioButtonLabeled to components index

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,9 @@
 
 // Atoms
 export { default as Button } from './atoms/Button/Button.jsx'
+export {
+  default as RadioButtonLabeled
+} from './atoms/RadioButtonLabeled/RadioButtonLabeled.jsx'
 
 // Molecules
 
