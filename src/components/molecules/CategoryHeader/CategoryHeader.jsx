7fe6a5827dfa8e4acf4eb177b73a74b1cef0468f
Judson Barroso <judsonbarroso@gmail.com>
2019-02-26 14:55:51 -0300

    feat(category-header): component and stories

---
+++ src/components/molecules/CategoryHeader/CategoryHeader.jsx
@@ -0,0 +1,55 @@
+/* @flow */
+/** @jsx jsx */
+import styled from '@emotion/styled'
+import { css, jsx } from '@emotion/core'
+import Container from '../../atoms/Container/Container'
+import Label from '../../atoms/Label/Label'
+
+type Props = {
+  /** Name of category */
+  title: string,
+  /** Subtitle */
+  subtitle: string,
+  /** Line */
+  hasLine?: boolean,
+  /** Dropshadow */
+  hasDropShadow?: boolean,
+  /** Content's components */
+  children?: () => mixed
+}
+
+const style = ({ theme }) => css`
+  text-align: center;
+  h1 {
+    font-family: ${theme.fontFamily.Fjalla};
+    font-size: ${theme.fontSize.Large};
+    text-transform: uppercase;
+  }
+`
+
+const StyledCategoryHeader = styled.div`
+  ${style}
+`
+
+const CategoryHeader = (props: Props) => {
+  const { title, subtitle, hasLine, children, ...others } = props
+  return (
+    <Container {...others}>
+      <StyledCategoryHeader>
+        <h1>{title}</h1>
+        <Label kind={'grizzly'} hasLine={hasLine}>
+          {subtitle}
+        </Label>
+        {children}
+      </StyledCategoryHeader>
+    </Container>
+  )
+}
+
+CategoryHeader.defaultProps = {
+  title: 'Categoria',
+  subtitle: 'Subtítulo',
+  hasLine: false
+}
+
+export default CategoryHeader
