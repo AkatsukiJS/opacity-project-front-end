b7090237168b96cf587a3326e8756b75f3c6d219
jeovazero <jeovapereira2000@live.com>
2019-04-14 19:42:59 -0300

    style: add global css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
+import { Global, css } from '@emotion/core'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const globalStyle = css`
+  body {
+    padding: 0;
+    margin: 0;
+  }
+`
+
+ReactDOM.render(
+  <>
+    <Global styles={globalStyle} />
+    <App />
+  </>,
+  document.getElementById('root')
+)
