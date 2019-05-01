7ff174387d7f3c153ae3cb26b33a753c5adbfd6d
jeovazero <jeovapereira2000@live.com>
2019-05-01 02:42:13 -0300

    feat(categories): add api calls

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,45 +1,83 @@
 // @flow
 /** @jsx jsx */
 
-import { Button, Label, CategoryHeader, Container, Select } from '../components'
+import {
+  Button,
+  Label,
+  CategoryHeader,
+  Container,
+  Select,
+  Loader
+} from '../components'
 import styled from '@emotion/styled'
 import { jsx } from '@emotion/core'
 import { useState } from 'react'
+import { withRouter } from 'react-router-dom'
+import { useApi, api } from '../client'
 
-const categoriesList = [
-  { label: 'Professores do magisterio superior', value: 'Categoria_A' },
-  { label: 'Categoria b', value: 'Categoria_B' },
-  { label: 'Categoria c', value: 'Categoria_C' },
-  { label: 'Categoria d', value: 'Categoria_D' },
-  { label: 'Categoria e', value: 'Categoria_E' }
-]
+const toCategoriesList = list => {
+  return list.map(el => ({
+    label: el.label,
+    value: el.key,
+    count: el.count
+  }))
+}
 
-const Categories = ({ className }) => {
+const SelectCategory = ({ onSelect, selected, setSelected }) => {
+  const { data, error, loading } = useApi(api.getCategories)
+
+  if (error) return <Label kind='primary'>{'Algum erro ocorreu! :('}</Label>
+  if (loading) return <Loader label='Carregando' />
+
+  const list = toCategoriesList(data)
+
+  return (
+    <Container className='op__categories__container' hasDropShadow={false}>
+      <div className='op__categories__title'>
+        <Label size='medium'>ESCOLHA UMA CATEGORIA</Label>
+      </div>
+      <div>
+        <Select
+          isBlock
+          options={list}
+          onSelect={t => setSelected(t)}
+          className='op__categories__select'
+          value={selected}
+          placeholder='Selecione'
+        />
+      </div>
+      <Button
+        hasDropShadow
+        size='medium'
+        onClick={() => selected !== 'none' && onSelect(selected)}
+      >
+        OK
+      </Button>
+    </Container>
+  )
+}
+
+const Categories = ({ className, history }) => {
   const [selected, setSelected] = useState('none')
+
   return (
     <div className={className}>
       <CategoryHeader
         title='SERVIDORES DA UFPI'
         subtitle='FONTE: Portal da Transparencia, Janeiro/2018'
       />
-      <Container className='op__categories__container' hasDropShadow={false}>
-        <div className='op__categories__title'>
-          <Label size='medium'>ESCOLHA UMA CATEGORIA</Label>
-        </div>
-        <div>
-          <Select
-            isBlock
-            options={categoriesList}
-            onSelect={t => setSelected(t)}
-            className='op__categories__select'
-            value={selected}
-            placeholder='Selecione'
-          />
-        </div>
-        <Button hasDropShadow size='medium'>
-          OK
-        </Button>
-      </Container>
+      <SelectCategory
+        selected={selected}
+        setSelected={setSelected}
+        onSelect={category =>
+          history.push({
+            pathname: '/category',
+            state: {
+              category
+            }
+          })
+        }
+      />
     </div>
   )
 }
@@ -59,4 +97,4 @@ const CategoriesStyled = styled(Categories)`
   }
 `
 
-export default CategoriesStyled
+export default withRouter(CategoriesStyled)
