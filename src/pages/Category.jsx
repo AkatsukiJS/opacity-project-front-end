a914cbc9b85722da328b483ce24826a7a8f6251e
jeovazero <jeovapereira2000@live.com>
2019-04-30 22:51:23 -0300

    feat(category): add a simple pagination

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -7,14 +7,18 @@ import {
   ActionsBar,
   DialogBoxModal,
   Button,
-  RadioGroup
+  RadioGroup,
+  Label
 } from '../components'
 import styled from '@emotion/styled'
 import { jsx } from '@emotion/core'
 import { useState } from 'react'
 import data from '../temp_data.json'
 
-const serverList = data.results
+const serverList = data.results.reduce(
+  (acc, cur) => [...acc, ...Array(40).fill(cur)],
+  []
+)
 const baseLink = 'https://portaldatransparencia.gov.br/servidores/'
 
 const transformServerData = raw => {
@@ -48,11 +52,36 @@ const transformServerData = raw => {
   ]
 }
 
+const pageSize = 5
+const totalItems = serverList.length
+
+const makePaginationArray = (sizeList, currentPage) => {
+  const size = Math.trunc(sizeList / pageSize) + (sizeList % pageSize !== 0)
+  if (size > 4) {
+    const prv = currentPage - 1
+    const nxt = currentPage + 1
+    const prvl = prv > 2 ? [NaN, prv] : prv === 2 ? [prv] : []
+    const nxtl = nxt < size - 1 ? [nxt, NaN] : nxt === size - 1 ? [nxt] : []
+    const curl = currentPage === 1 || currentPage === size ? [] : [currentPage]
+    return [1, ...prvl, ...curl, ...nxtl, size]
+  } else {
+    return [1, 2, 3, 4]
+  }
+}
+
+const getPage = (list, pageNumber, pageSize) => {
+  const offset = pageSize * pageNumber
+  const end = offset + pageSize
+  return list.slice(offset, end)
+}
+
 const Category = ({ className }) => {
   const [isOpenModal, setOpenModal] = useState(false)
   const [selectedSort, setSelectedSort] = useState('Name')
   const [selectedOrder, setSelectedOrder] = useState('Crescente')
-
+  const [currentPage, setCurrentPage] = useState(0)
+  const page = currentPage + 1
+  const list = getPage(serverList, currentPage, pageSize)
   return (
     <div className={className}>
       <CategoryHeader
@@ -73,7 +102,7 @@ const Category = ({ className }) => {
         />
       </div>
       <div className='op__category__container'>
-        {serverList.map((el, i) => (
+        {list.map((el, i) => (
           <ServerCard
             key={i}
             className='op__category__servercard'
@@ -83,6 +112,25 @@ const Category = ({ className }) => {
             isBlock
           />
         ))}
+      </div>
+      <div className='op__category__pagination'>
+        {makePaginationArray(totalItems, page).map((el, key) => {
+          const [child, canHandler] = isNaN(el)
+            ? ['...', false]
+            : [`${el}`, true]
+          const kind = el === page ? 'primary' : 'basic'
+          return (
+            <div
+              className='op__category__pagenumber'
+              key={key}
+              onClick={() => canHandler && setCurrentPage(el - 1)}
+            >
+              <Label size='large' kind={kind}>
+                {child}
+              </Label>
+            </div>
+          )
+        })}
       </div>
       <DialogBoxModal
         title='Ordenar'
@@ -110,7 +158,7 @@ const Category = ({ className }) => {
             </div>
           </div>
           <div className='op__dialogmodal__button'>
-            <Button hasDropShadow>OK</Button>
+            <Button hasDropShadow>Ordenar</Button>
           </div>
         </div>
       </DialogBoxModal>
@@ -134,6 +182,15 @@ const CategoryStyled = styled(Category)`
     margin: 16px 0 0;
     text-align: center;
     display: block;
+  }
+  .op__category__pagination {
+    display: flex;
+    justify-content: center;
+    padding: 1rem 0 2rem;
+  }
+  .op__category__pagenumber {
+    cursor: pointer;
+    margin: 0 8px;
   }
 `
 
